f9174ef918ee77f7f9a28968d9710062e1741fb8
Shabaz123 <shahbaziub1377@gmail.com>
2024-01-06 15:56:36 +0500

    first comit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import Name from "./react17/name";
+
+function App(){
+
+
+  return(
+<div>
+  <Name title="shahbaz" class1="12"/>
+  <Name title= "ali" section1="A"/>
+  <Name title= "azan" roll1 = "32"/>
+  <Name title= "haider" address1= "lahore"/>
+  <Name title="gaza" email1="kak@gmail.com"/>
+</div>
+
+
+  )
+}
+
+
+export default App
---
+++ src/react17/name.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Name({title , class1 , section1}){
+    return(
+        <div>
+
+<h1>hy{title} your class is{class1} </h1>
+<h2>hy {title} your section is{section1}</h2>
+
+
+        </div>
+
+
+
+
+
+
+
+
+
+    )
+    }
+
+export default Name
